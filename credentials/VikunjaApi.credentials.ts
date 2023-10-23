96cb7f44004f6cbf8cb5518b48da27e01db896fc
kolaente <k@knt.li>
2023-10-23 11:10:07 +0200

    feat: add vikunja credentials

---
+++ credentials/VikunjaApi.credentials.ts
@@ -0,0 +1,27 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class VikunjaApi implements ICredentialType {
+	name = 'vikunjaApi';
+	displayName = 'Vikunja API';
+	documentationUrl = 'https://vikunja.io/docs/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			default: '',
+		},
+	];
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: 'Bearer ={{$credentials.apiKey}}'
+			}
+		},
+	};
+}
